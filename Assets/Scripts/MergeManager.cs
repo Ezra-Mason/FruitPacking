d7fe1e52bdeeb27ab20f8ff5f81df18d58d2dee3
EZ-LAPTOP\ezram <ezra.mason@outlook.com>
2026-01-29 23:10:06 +0000

    feat: add spawning new fruit after merging

--- Assets/Scripts/MergeManager.cs
+++ Assets/Scripts/MergeManager.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using ezutils.Core;
+using System;
 
 public class MergeManager : MonoBehaviour
 {
+    [SerializeField] private GameObjectRepository _alllFruit;
     private List<Fruit> _fruit = new List<Fruit>();
+
     public void AddFruit(Fruit fruit)
     {
         _fruit.Add(fruit);
@@ -13,12 +17,19 @@ public class MergeManager : MonoBehaviour
     private void HandleCollision(Fruit fruit, Collision2D collision)
     {
         collision.gameObject.TryGetComponent<Fruit>(out var otherfruit);
-        if (otherfruit != null)
+        if (otherfruit == null) return;
+
+        if (otherfruit.Size == fruit.Size)
         {
-            if (otherfruit.Size == fruit.Size)
+            RemoveFruit(fruit);
+            RemoveFruit(otherfruit);
+            if (fruit.Size + 1 < _alllFruit.Length )
             {
-                RemoveFruit(fruit);
-                RemoveFruit(otherfruit);
+                SpawnNewFruit(fruit.Size + 1, collision.contacts[0].point);
+            }
+            else
+            {
+                Debug.Log("Max limit of merged fruit");
             }
         }
     }
@@ -29,5 +40,19 @@ public class MergeManager : MonoBehaviour
         fruit.OnFruitCollision -= HandleCollision;
         Destroy(fruit.gameObject);
     }
+    private void SpawnNewFruit(int size, Vector2 position)
+    {
+        var newFruitGO = Instantiate(_alllFruit[size], new Vector3(position.x, position.y, 0f), Quaternion.identity);
+        newFruitGO.TryGetComponent<Fruit>(out var fruit);
+        if (fruit != null)
+        {
+            AddFruit(fruit);
+        }
+        newFruitGO.TryGetComponent<Rigidbody2D>(out var rb);
+        if (rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
 
+        }
+    }
 }
